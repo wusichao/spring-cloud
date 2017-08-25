58fcfcbef273d21d2d60f551403157a9c7b372c0
wusc <1033429246@qq.com>
2017-08-25 16:50:23 +0800

    去警告

--- consumer-one/src/main/java/com/wusc/consumerone/service/HelloService.java
+++ consumer-one/src/main/java/com/wusc/consumerone/service/HelloService.java
@@ -5,8 +5,6 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
 @Service
 public class HelloService {
 	@Autowired
