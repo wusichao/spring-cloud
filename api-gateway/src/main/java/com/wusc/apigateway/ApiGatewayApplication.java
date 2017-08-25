ca16d20d728e99af4bf6fdf9ba4d3695a4e549e2
wusc <1033429246@qq.com>
2017-08-26 00:23:21 +0800

    idea git 测试

--- api-gateway/src/main/java/com/wusc/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/wusc/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class ApiGatewayApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
