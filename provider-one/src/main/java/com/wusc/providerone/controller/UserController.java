20b78196260124c1033da47a0d741f27903e6069
wusc <1033429246@qq.com>
2017-08-29 13:04:25 +0800

    测试自动更新配置

--- provider-one/src/main/java/com/wusc/providerone/controller/UserController.java
+++ provider-one/src/main/java/com/wusc/providerone/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.wusc.providerone.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,7 @@ import com.wusc.providerone.service.UserService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-
+@RefreshScope
 @RestController
 @RequestMapping(produces="application/json")
 public class UserController {
@@ -23,6 +25,8 @@ public class UserController {
 	private UserService userService;
 	@Autowired
 	private DiscoveryClient discoveryClient;
+	@Value("${from}")
+	private String from;
 	@ApiOperation(value="根据名字获取用户", response=String.class)
 	@ApiImplicitParams({
 		@ApiImplicitParam(dataType="String", paramType="header", value="令牌", name="token", required=true),
@@ -33,5 +37,10 @@ public class UserController {
 		ServiceInstance instance = discoveryClient.getLocalServiceInstance();
 		System.out.println(instance.getHost()+instance.getPort()+instance.getServiceId());
 		return ResponseEntity.ok(new ModelMap().addAttribute("data", userService.say(name)).addAttribute("by", instance.getPort()+instance.getServiceId()));
+	}
+	@RequestMapping(value = "from",method = RequestMethod.GET)
+	public ResponseEntity<ModelMap> from(){
+
+	return ResponseEntity.ok(new ModelMap().addAttribute("data",from));
 	}
 }
